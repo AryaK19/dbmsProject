be944b5a77ec6e2d0832896d6af5bae56f6189ff
AryaK19 <aryakadam348@gmail.com>
2024-10-15 09:13:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -115,7 +115,6 @@ app.get('/hackathons', (req, res) => {
     res.status(200).json(results);
   });
 });
-
 // Start server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
